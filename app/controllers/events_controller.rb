23ea99065bc0e9568b85c0a37eb06d19002f518a
abruckman <abruckman09@gmail.com>
2017-08-29 16:32:28 -0700

    seems to work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @graph = Koala::Facebook::API.new()
-    @me = @graph.get_object("me")
-    @friends = @graph.get_connections("me", "permissions")
-    p "$"*50
-    p @me
-    p @friends
+    # @graph = Koala::Facebook::API.new()
+    # @me = @graph.get_object("me")
+    # @friends = @graph.get_connections("me", "permissions")
+    # p "$"*50
+    # p @me
+    # p @friends
 
 
   end
